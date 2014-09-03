457af8fececbb3c428e51138bde09f76a02aa187
xecus-mba <naisyo@naisyo.com>
2014-09-03 14:42:12 +0900

    Add CPU Mode

--- main.cpp
+++ main.cpp
@@ -4,22 +4,39 @@
 #include <fstream>
 
 //CUDA
-#include <cuda_runtime.h>
-#include <cusparse_v2.h>
-#include <cublas_v2.h>
-#include <helper_functions.h>
-#include <helper_cuda.h>
+#ifdef ENABLE_CUDA
+	#include <cuda_runtime.h>
+	#include <cusparse_v2.h>
+	#include <cublas_v2.h>
+	#include <helper_functions.h>
+	#include <helper_cuda.h>
+#endif
 
 template <typename TYPE> class VAR{
 private:
 public:
 	TYPE *h;
+	#ifdef ENABLE_CUDA
 	cublasHandle_t cublasHandle;
 	cublasStatus_t cublasStatus;
+	#endif
 	int M;
 	int N;
 	int NumOfEle;
 	bool TypeOfIndex;
+
+	//Convert Index
+	int ConvertIndex(int _row,int _col){
+		int index;
+		if(this->TypeOfIndex)
+			index = _col + _row * this->N;
+		else
+			index = _row + _col * this->M;
+		if(index<0 || index>(this->NumOfEle)){
+			exit(EXIT_FAILURE);
+		}
+		return index;
+	}
 
 	//Product
 	double operator % (VAR obj) {
@@ -28,17 +45,25 @@ public:
 		//Vector Only
 		if(this->M==obj.M && this->N==obj.N){
 			int NNN = this->M * this->N;
-			double *dA,*dB;
-			checkCudaErrors(cudaMalloc((void **)&dA, sizeof(double)*NNN));
-			checkCudaErrors(cudaMalloc((void **)&dB, sizeof(double)*NNN));
-			cudaMemcpy(dA, this->h , sizeof(double)*NNN, cudaMemcpyHostToDevice);
-			cudaMemcpy(dB, obj.h , sizeof(double)*NNN, cudaMemcpyHostToDevice);
-			cublasDdot(cublasHandle, NNN, dA, 1, dB, 1, &ResDot);
-			cudaError_t err;
-			err=cudaFree(dA);
-			if (err != cudaSuccess) std::cout << "error:" << cudaGetErrorString(err) << std::endl;
-			err=cudaFree(dB);
-			if (err != cudaSuccess) std::cout << "error:" << cudaGetErrorString(err) << std::endl;
+			#ifdef ENABLE_CUDA
+				/* Calc on GPU */
+				double *dA,*dB;
+				checkCudaErrors(cudaMalloc((void **)&dA, sizeof(double)*NNN));
+				checkCudaErrors(cudaMalloc((void **)&dB, sizeof(double)*NNN));
+				cudaMemcpy(dA, this->h , sizeof(double)*NNN, cudaMemcpyHostToDevice);
+				cudaMemcpy(dB, obj.h , sizeof(double)*NNN, cudaMemcpyHostToDevice);
+				cublasDdot(cublasHandle, NNN, dA, 1, dB, 1, &ResDot);
+				cudaError_t err;
+				err=cudaFree(dA);
+				if (err != cudaSuccess) std::cout << "error:" << cudaGetErrorString(err) << std::endl;
+				err=cudaFree(dB);
+				if (err != cudaSuccess) std::cout << "error:" << cudaGetErrorString(err) << std::endl;
+			#else
+				/* Calc on CPU */
+				for(int i=0;i<NNN;i++)
+					ResDot += this->h[i] * obj.h[i];
+			#endif
+
 		}else{
 			std::cout << "Dimension Error!" << std::endl;
 		}
@@ -50,20 +75,25 @@ public:
 		//for(int i=0;i<(this->NumOfEle);i++) res.h[i] -= obj.h[i];
 		if(this->M==obj.M && this->N==obj.N){
 			int NNN = this->M * this->N;
-			double alpha = -1.0;
-			double *dA,*dB;
-			checkCudaErrors(cudaMalloc((void **)&dA, sizeof(double)*NNN));
-			checkCudaErrors(cudaMalloc((void **)&dB, sizeof(double)*NNN));
-			cudaMemcpy(dA, this->h , sizeof(double)*NNN, cudaMemcpyHostToDevice);
-			cudaMemcpy(dB, obj.h , sizeof(double)*NNN, cudaMemcpyHostToDevice);
-			// dA = dA + dB
-			cublasDaxpy(cublasHandle, NNN, &alpha, dB, 1, dA, 1);
-			cudaMemcpy(res.h, dA , sizeof(double)*NNN, cudaMemcpyDeviceToHost);
-			cudaError_t err;
-			err=cudaFree(dA);
-			if (err != cudaSuccess) std::cout << "error:" << cudaGetErrorString(err) << std::endl;
-			err=cudaFree(dB);
-			if (err != cudaSuccess) std::cout << "error:" << cudaGetErrorString(err) << std::endl;
+			#ifdef ENABLE_CUDA
+				double alpha = -1.0;
+				double *dA,*dB;
+				checkCudaErrors(cudaMalloc((void **)&dA, sizeof(double)*NNN));
+				checkCudaErrors(cudaMalloc((void **)&dB, sizeof(double)*NNN));
+				cudaMemcpy(dA, this->h , sizeof(double)*NNN, cudaMemcpyHostToDevice);
+				cudaMemcpy(dB, obj.h , sizeof(double)*NNN, cudaMemcpyHostToDevice);
+				// dA = dA + dB
+				cublasDaxpy(cublasHandle, NNN, &alpha, dB, 1, dA, 1);
+				cudaMemcpy(res.h, dA , sizeof(double)*NNN, cudaMemcpyDeviceToHost);
+				cudaError_t err;
+				err=cudaFree(dA);
+				if (err != cudaSuccess) std::cout << "error:" << cudaGetErrorString(err) << std::endl;
+				err=cudaFree(dB);
+				if (err != cudaSuccess) std::cout << "error:" << cudaGetErrorString(err) << std::endl;
+			#else
+				for(int i=0;i<NNN;i++)
+					res.h[i] = this->h[i] - obj.h[i];
+			#endif
 		}else{
 			std::cout << "Dimension Error!" << std::endl;
 		}
@@ -75,20 +105,25 @@ public:
 		//for(int i=0;i<(this->NumOfEle);i++) res.h[i] -= obj.h[i];
 		if(this->M==obj.M && this->N==obj.N){
 			int NNN = this->M * this->N;
-			double alpha = +1.0;
-			double *dA,*dB;
-			checkCudaErrors(cudaMalloc((void **)&dA, sizeof(double)*NNN));
-			checkCudaErrors(cudaMalloc((void **)&dB, sizeof(double)*NNN));
-			cudaMemcpy(dA, this->h , sizeof(double)*NNN, cudaMemcpyHostToDevice);
-			cudaMemcpy(dB, obj.h , sizeof(double)*NNN, cudaMemcpyHostToDevice);
-			// dA = dA + dB
-			cublasDaxpy(cublasHandle, NNN, &alpha, dB, 1, dA, 1);
-			cudaMemcpy(res.h, dA , sizeof(double)*NNN, cudaMemcpyDeviceToHost);
-			cudaError_t err;
-			err=cudaFree(dA);
-			if (err != cudaSuccess) std::cout << "error:" << cudaGetErrorString(err) << std::endl;
-			err=cudaFree(dB);
-			if (err != cudaSuccess) std::cout << "error:" << cudaGetErrorString(err) << std::endl;
+			#ifdef ENABLE_CUDA
+				double alpha = +1.0;
+				double *dA,*dB;
+				checkCudaErrors(cudaMalloc((void **)&dA, sizeof(double)*NNN));
+				checkCudaErrors(cudaMalloc((void **)&dB, sizeof(double)*NNN));
+				cudaMemcpy(dA, this->h , sizeof(double)*NNN, cudaMemcpyHostToDevice);
+				cudaMemcpy(dB, obj.h , sizeof(double)*NNN, cudaMemcpyHostToDevice);
+				// dA = dA + dB
+				cublasDaxpy(cublasHandle, NNN, &alpha, dB, 1, dA, 1);
+				cudaMemcpy(res.h, dA , sizeof(double)*NNN, cudaMemcpyDeviceToHost);
+				cudaError_t err;
+				err=cudaFree(dA);
+				if (err != cudaSuccess) std::cout << "error:" << cudaGetErrorString(err) << std::endl;
+				err=cudaFree(dB);
+				if (err != cudaSuccess) std::cout << "error:" << cudaGetErrorString(err) << std::endl;
+			#else
+				for(int i=0;i<NNN;i++)
+					res.h[i] = this->h[i] + obj.h[i];
+			#endif
 		}else{
 			std::cout << "Dimension Error!" << std::endl;
 		}
@@ -107,37 +142,37 @@ public:
 			int M = this->M;
 			int N = obj.N;
 			int K = obj.M;
-			double *hA,*hB,*hC;
-			double *dA,*dB,*dC;
-			hA = this->h;
-			hB = obj.h;
-			hC = res.h;
-			checkCudaErrors(cudaMalloc((void **)&dA, sizeof(double)*M*K));
-			checkCudaErrors(cudaMalloc((void **)&dB, sizeof(double)*K*N));
-			checkCudaErrors(cudaMalloc((void **)&dC, sizeof(double)*M*N));
-			cudaMemcpy(dA, hA , sizeof(double)*M*K, cudaMemcpyHostToDevice);
-			cudaMemcpy(dB, hB , sizeof(double)*K*N, cudaMemcpyHostToDevice);
-			cudaMemcpy(dC, hC , sizeof(double)*M*N, cudaMemcpyHostToDevice);
-			double alpha = 1.0 , beta = 0.0;
-			cublasDgemm(cublasHandle,CUBLAS_OP_N,CUBLAS_OP_N,M,N,K,&alpha,dA,M,dB,K,&beta,dC,M);
-			cudaMemcpy(hC, dC , sizeof(double)*M*N, cudaMemcpyDeviceToHost);
+			#ifdef ENABLE_CUDA
+				double *hA,*hB,*hC;
+				double *dA,*dB,*dC;
+				hA = this->h;
+				hB = obj.h;
+				hC = res.h;
+				checkCudaErrors(cudaMalloc((void **)&dA, sizeof(double)*M*K));
+				checkCudaErrors(cudaMalloc((void **)&dB, sizeof(double)*K*N));
+				checkCudaErrors(cudaMalloc((void **)&dC, sizeof(double)*M*N));
+				cudaMemcpy(dA, hA , sizeof(double)*M*K, cudaMemcpyHostToDevice);
+				cudaMemcpy(dB, hB , sizeof(double)*K*N, cudaMemcpyHostToDevice);
+				cudaMemcpy(dC, hC , sizeof(double)*M*N, cudaMemcpyHostToDevice);
+				double alpha = 1.0 , beta = 0.0;
+				cublasDgemm(cublasHandle,CUBLAS_OP_N,CUBLAS_OP_N,M,N,K,&alpha,dA,M,dB,K,&beta,dC,M);
+				cudaMemcpy(hC, dC , sizeof(double)*M*N, cudaMemcpyDeviceToHost);
+			#else
+				for(int ii=0;ii<M;ii++){
+					for(int jj=0;jj<N;jj++){
+						double temp = 0.0;
+						for(int kk=0;kk<K;kk++)
+							temp+= this->h[ this->ConvertIndex(ii,kk) ] * obj.h[ obj.ConvertIndex(kk,jj) ];
+						res.h[ res.ConvertIndex(ii,jj) ] = temp;
+					}
+				}
+			#endif
 		}else{
 			std::cout << "Dimension Error!" << std::endl;
 		}
 		return res;
 	}
 
-	int ConvertIndex(int _row,int _col){
-		int index;
-		if(this->TypeOfIndex)
-			index = _col + _row * this->N;
-		else
-			index = _row + _col * this->M;
-		if(index<0 || index>(this->NumOfEle)){
-			exit(EXIT_FAILURE);
-		}
-		return index;
-	}
 	TYPE& operator () (int _row) {
 		int index = ConvertIndex(_row,0);
 		return this->h[index];
@@ -152,9 +187,11 @@ public:
 		this->N = _N;
 		this->NumOfEle = _M * _N;
 		this->TypeOfIndex = false;
-		this->cublasHandle = 0;
-		this->cublasStatus = cublasCreate(&this->cublasHandle);
-		if (checkCudaErrors(this->cublasStatus)) exit(EXIT_FAILURE);
+		#ifdef ENABLE_CUDA
+			this->cublasHandle = 0;
+			this->cublasStatus = cublasCreate(&this->cublasHandle);
+			if (checkCudaErrors(this->cublasStatus)) exit(EXIT_FAILURE);
+		#endif
 		this->h = new TYPE[ this->NumOfEle ];
 		return;
 	}
@@ -163,8 +200,10 @@ public:
 		this->N = obj.N;
 		this->NumOfEle = obj.NumOfEle;
 		this->TypeOfIndex = obj.TypeOfIndex;
-		this->cublasHandle = obj.cublasHandle;
-		this->cublasStatus = obj.cublasStatus;
+		#ifdef ENABLE_CUDA
+			this->cublasHandle = obj.cublasHandle;
+			this->cublasStatus = obj.cublasStatus;
+		#endif
 		this->h = new TYPE[ this->NumOfEle ];
 		for(int i=0;i<(this->NumOfEle);i++) this->h[i] = obj.h[i];
 		return;
@@ -247,7 +286,7 @@ int main() {
 	B(1,0) = +3.0;
 	B(1,1) = +4.0;
 
-	C = A * B;
+	C = A + B;
 	C.SaveCsv("a.csv");
 
 	return 0;
